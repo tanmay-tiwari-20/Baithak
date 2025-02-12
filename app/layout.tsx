ad327cf70dfd9d3744c158d3722a49849f8b61ee
Tanmay Tiwari <tanmaytiwari200905@gmail.com>
2025-02-12 18:46:29 +0530

    Stream provider added Meeting room creating successfully

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +46,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}
         >
           {children}
+          <Toaster />
         </body>
       </ClerkProvider>
     </html>
