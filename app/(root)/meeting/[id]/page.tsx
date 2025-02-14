a96dfef1e2171d83f895851f5a0e9fe472153a35
Tanmay Tiwari <tanmaytiwari200905@gmail.com>
2025-02-14 20:33:06 +0530

    Still debugging

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,14 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
+import { FC, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import Loader from "@/components/Loader";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
-import { useGetCallById } from "@/hooks/useGetCallById";
-import { useUser } from "@clerk/nextjs";
-import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-import React, { useState } from "react";
 
-const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+interface MeetingPageProps {
+  params: { id: string };
+}
+
+const Meeting: FC<MeetingPageProps> = ({ params }) => {
+  const { id } = params;
   const { user, isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   const { call, isCallLoading } = useGetCallById(id);
@@ -19,7 +23,11 @@ const Meeting = ({ params: { id } }: { params: { id: string } }) => {
     <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? <MeetingSetup setIsSetupComplete={setIsSetupComplete} /> : <MeetingRoom />}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
