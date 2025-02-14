63e2decf70ddd22ae42915a605c0f0734ee4b8c5
Tanmay Tiwari <tanmaytiwari200905@gmail.com>
2025-02-14 22:12:35 +0530

    solving

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,20 +1,43 @@
 "use client";
+
 import { useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
+import { Loader } from "lucide-react";
+
 import { useGetCallById } from "@/hooks/useGetCallById";
-import Loader from "@/components/Loader";
-import MeetingRoom from "@/components/MeetingRoom";
+import Alert from "@/components/Alert";
 import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
 
-
-const Meeting = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  const { isLoaded } = useUser();
-  const [isSetupComplete, setIsSetupComplete] = useState(false);
+const MeetingPage = () => {
+  const params = useParams();
+  const id = params?.id as string; // Ensure `id` is a string
+  const { isLoaded, user } = useUser();
   const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
 
-  if (!isLoaded || isCallLoading) return <Loader />;
+  if (!isLoaded || isCallLoading)
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Loader className="w-10 h-10 text-white animate-spin" />
+      </div>
+    );
+
+  if (!call)
+    return (
+      <p className="text-center text-3xl font-bold text-white">
+        Call Not Found
+      </p>
+    );
+
+  const isUserAllowed =
+    call.type !== "invited" ||
+    (user && call.state?.members?.some((m) => m.user?.id === user.id));
+
+  if (!isUserAllowed)
+    return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
@@ -31,4 +54,4 @@ const Meeting = ({ params }: { params: { id: string } }) => {
   );
 };
 
-export default Meeting;
+export default MeetingPage;
